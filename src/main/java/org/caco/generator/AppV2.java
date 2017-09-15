8489e475811c312308415d415e25cc427937dc31
roach <roach@qq.com>
2017-09-15 15:56:25 +0800

    up

--- src/main/java/org/caco/generator/AppV2.java
+++ src/main/java/org/caco/generator/AppV2.java
@@ -1,7 +1,97 @@
 package org.caco.generator;
 
+import org.caco.generator.bean.Config;
+import org.caco.generator.bean.Table;
+import org.caco.generator.util.CodeUtil;
+import org.caco.generator.util.DBHelper;
+
+import java.util.Map;
+
 /**
+ * 一次多个数据生成
  * created by roachjiang 2017/9/14
  */
 public class AppV2 {
+    public static void main(String[] args) {
+
+        Map<String,Config> beans = BeanFactory.getConfigBean();
+        for (  Map.Entry<String,Config> config : beans.entrySet()){
+            System.out.println("beanName : " + config.getKey());
+            generatorCodeForBean(config.getValue());
+        }
+    }
+
+    private static void generatorCodeForBean(Config config) {
+        DBHelper dbHelper = (DBHelper) BeanFactory.getBean("mySqlHelper");
+        Table t = dbHelper.getTable(config.getTableName());
+        t.setEntityName(config.getEntityName());
+        t.setProject(config.getModelName());
+        t.setModule(config.getPakeage());
+        t.setSimpleModule(t.getModule().substring(t.getModule().lastIndexOf(".") + 1));
+        t.setAuthor(config.getAuthor());
+        t.setEqualsSearchColumns(config.getEqualsSearchColumns());
+        t.setLikeSearchColumns(config.getLikeSearchColumns());
+
+        //如果在栏位面前添加"!"符号则代表设置此栏位显示
+        //String defaulHidePOColumns = "code,name,version,currentUser,currentUserId,status,ifDel,createUser,createDate,modifyUser,modifyDate";
+        //t.setHidePOColumns(defaulHidePOColumns);
+        t.setHidePOColumns(config.getHidePOColumns());
+
+        String defaulHideGridColumns = "cid";
+        t.setHideGridColumns(defaulHideGridColumns);
+        t.setHideGridColumns(config.getHideGridColumns());
+
+        try {
+            t.setFKColumns(config.getFkColumns());//外键关联
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        try {
+            t.setRenderColumns(config.getRenderColumns());//code转text
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        try {
+            t.setDictColumns(config.getDictColumns());//下拉绑定
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        try {
+            t.setDictAllColumns(config.getDictAllColumns());//下拉绑定，包含all选项
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        gerenateCode(t, BeanFactory.getClassPath() + "/generated-sources");
+        System.out.println("Generated success:" + BeanFactory.getClassPath() + "\\generated-sources");
+    }
+
+    /**
+     * 代码生成
+     *
+     * @param table
+     * @param path
+     */
+    public static void gerenateCode(Table table, String path) {
+        if (path.endsWith("/"))
+            path = path.substring(0, path.length() - 1);
+        String servicePath = path + "/" + table.getProject() + "/" + table.getModule().replace(".", "/");
+        String serviceImplPath = path + "/" + table.getProject()  + "/"+ table.getModule().replace(".", "/");
+        String controllerPath = path + "/" + table.getProject() + "/controller/" + table.getModule().replace(".", "/");
+        String xmlPath = path + "/" + table.getProject() + "/" + table.getModule().replace(".", "/");
+        String jspPath = path + "/" + table.getProject() + "/jsp/" + table.getProject() + "/";
+        String curPath = path + "/" + table.getProject() + "/";
+
+        CodeUtil.genereateCode("service.ftl", table, servicePath + "/service/I" + table.getEntityName() + "Service.java");
+        CodeUtil.genereateCode("po.ftl", table, servicePath + "/model/" + table.getEntityName() + "PO.java");
+
+        CodeUtil.genereateCode("mapper.ftl", table, serviceImplPath + "/mapper/" + table.getEntityName() + "Mapper.java");
+        CodeUtil.genereateCode("service_impl.ftl", table, serviceImplPath + "/service/impl/" + table.getEntityName() + "ServiceImpl.java");
+        CodeUtil.genereateCode("controller.ftl", table, controllerPath + "/controller/" + table.getEntityName() + "Controller.java");
+        CodeUtil.genereateCode("mapper_xml.ftl", table, xmlPath + "/mapper/" + table.getEntityName() + "Mapper.xml");
+        CodeUtil.genereateCode("jsp.ftl", table, jspPath + "/ey_" + CodeUtil.toLowerCaseFirstChar(table.getEntityName()) + ".jsp");
+        CodeUtil.genereateCode("sql.ftl", table, curPath + "/seq.sql");
+
+    }
 }
--- src/main/java/org/caco/generator/BeanFactory.java
+++ src/main/java/org/caco/generator/BeanFactory.java
@@ -1,15 +1,17 @@
 package org.caco.generator;
 
 import java.io.IOException;
+import java.util.Map;
 
+import org.caco.generator.bean.Config;
 import org.springframework.beans.factory.support.DefaultListableBeanFactory;
 import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
 import org.springframework.core.io.ClassPathResource;
 
 public class BeanFactory {
-	 static ClassPathResource resource = new ClassPathResource("generatorConfig.xml");  
-	 static DefaultListableBeanFactory factory = new DefaultListableBeanFactory();  
-	 static XmlBeanDefinitionReader reader =  new XmlBeanDefinitionReader(factory);  
+	 static ClassPathResource resource = new ClassPathResource("generatorConfig_riskManagement.xml");
+	 static DefaultListableBeanFactory factory = new DefaultListableBeanFactory();
+	 static XmlBeanDefinitionReader reader =  new XmlBeanDefinitionReader(factory);
      static{
     	 reader.loadBeanDefinitions(resource);
      }
@@ -17,8 +19,14 @@ public class BeanFactory {
      public static Object getBean(String beanId) {
     	 return factory.getBean(beanId);
      }
-     
-     public static String getClassPath() {
+
+	public static Map<String, Config> getConfigBean() {
+		Map<String, Config> beans = factory.getBeansOfType(Config.class);
+		return beans;
+	}
+
+
+	public static String getClassPath() {
     	 try {
 			String path =  resource.getFile().toString();
 			path = path.substring(0, path.indexOf("target")+"target".length());
